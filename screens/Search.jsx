8d0339b19c44227b7126c231cffea170bca95160
Keldrick <kelloderrick48@gmail.com>
2024-11-28 02:34:38 +0000

    Android Platform

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Platform,
+} from "react-native";
 
 const Search = ({ navigation }) => {
   return (
-    <SafeAreaView>
-      <View className="flex-1 justify-center items-center bg-gray-100">
+    // Conditionally render SafeAreaView based on platform
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F8F8F8" }}>
+      <View
+        className="flex-1 justify-center items-center bg-gray-100"
+        style={{ paddingTop: Platform.OS === "android" ? 30 : 0 }} // Add padding-top for Android
+      >
         <TouchableOpacity
           onPress={() => navigation.navigate("Onboarding")} // Navigate to the onboarding screen
           className="px-4 py-2 bg-blue-500 rounded-full"
